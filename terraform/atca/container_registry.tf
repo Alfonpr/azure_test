cf1df2e4da649a90890d69e95660a1c07d7c4a4f
Alfonso Palomino Romero <apalomro@emeal.nttdata.com>
2023-05-10 14:31:04 +0200

    Commented container registry to make a first deployment on github actions

--- terraform/atca/container_registry.tf
+++ terraform/atca/container_registry.tf
@@ -1,3 +1,4 @@
+/*
 resource "azurerm_container_registry" "atca_cr" {
   name                = var.atca_cr_name
   resource_group_name = azurerm_resource_group.example.name
@@ -18,3 +19,4 @@ resource "azurerm_container_registry_task" "atca_crt" {
     context_access_token = var.atca_crt_docker_step_context_access_token
   }
 }
+*/
